c240591f0655d0c113ecaa52aeb53a094c292628
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-03-11 09:22:17 -0400

    3/11/25 no idea

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 ## This function opens the CSV for You!
-def csv_to_list(file_path):
+""" def csv_to_list(file_path):
     data_list = []
     
     with open(file_path, 'r') as file:
@@ -13,9 +13,22 @@ def csv_to_list(file_path):
 
 file_path = "SalesData.csv"  
 data = csv_to_list(file_path)
-print(data)  # Output the list
+print(data)  # Output the list """
+
+def alltemp():
+    celcius = {}
+    for temp in temperatures[1:]:
+        temperatures = ["Label", 32, 50, 77, 104]
+        "Label" == temp[0]
+        celcius = map(int, temp[1:])
+        celcius["Label"] = ((temperatures + 32) * 4 ) % 9
+    print(celcius)
+    
+
+        
 
 
+""" ( C = (F - 32) * frac{5}{9} ) """
 
 
     
