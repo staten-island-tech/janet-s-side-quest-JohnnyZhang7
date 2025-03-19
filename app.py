e68a541500226d10d00f6ce14b2c57a2e68254d7
JohnnyZhang7 <zjohnnyzhang@gmail.com>
2025-03-19 09:05:19 -0400

    3/19/25 bugged

--- app.py
+++ app.py
@@ -30,7 +30,6 @@ for dat in data[1:]:
 
 
 
-
 """ def alltemp():
     for temp in temperatures[1:]: #doesn't work
         temperatures = ["labels", 32, 50, 77, 104]
@@ -46,36 +45,8 @@ alltemp() """
 #x[2]y [2]y
 
 #Lambda function is an anonymous function, it has no name. We use it inside map to condense the map function. You can just say lambda, create this instead of giving something a name and then doing the same thing. We don't have to write return too so it simplifies.
-        
 
-""" while True:
-    course_choice = print(str(input(f"What course would you like to enroll in?")))
-    fee = 0
-    if course_choice == "Python Programming":
-        print("No more seats, would you like to enroll in Machine Learning or Cybersecurity?")
-    elif course_choice == "Machine Learning":
-        fee += 200
-        print(f"Your total is now ${fee}")
-    elif course_choice == "Business Strategy":
-        fee += 100
-        print(f"Your total is now ${fee}")
-    elif course_choice  == "Marketing 101": 
-        fee += 120
-        print(f"Your total is now ${fee}")
-    elif course_choice == "Cybersecurity":
-        fee += 180
-        print(f"Your total is now ${fee}")
-    checkout = print(input(f"Would you like to check out?"))
-    if checkout == "no":
-        course_choice = print(input(f"What course would you like to enroll in?"))
 
-    if checkout == "yes":
-        print(f"Your total payment without discount is ${"fee"}")
-        if course_choice == "Machine Learning" or "Cybersecurity":
-            fee -= 20
-        elif course_choice >= 3:
-            fee * 0.95
-    break """
 
 courses = {
     "Python Programming": {"fee": 150, "seats": 0, "category": "Technology"},
@@ -84,8 +55,9 @@ courses = {
     "Marketing 101": {"fee": 120, "seats": 2, "category": "Business"},
     "Cybersecurity": {"fee": 180, "seats": 4, "category": "Technology"}
 }
+fee = 0
+print("hi")
 while True:
-    fee = 0
     Python_Programming = courses["Python Programming"]
     Machine_Learning = courses["Machine Learning"]
     Business_Strategy = courses["Business Strategy"]
@@ -97,31 +69,31 @@ while True:
         if Python_Programming["seats"] == 0:
             print("Python Programming is full. Suggesting Machine Learning or Cybersecurity instead")
         else:
-            fee + Python_Programming["fee"]
-
+            fee += Python_Programming["fee"]
+        
     elif student_enrollment == "Machine Learning":
         if Machine_Learning["seats"] == 0:
             print("Machine Learning is full. Suggesting Python Programming or Cybersecurity instead")
         else:
-            fee + Machine_Learning["fee"]
+            fee += Machine_Learning["fee"]
 
     elif student_enrollment == "Business Strategy":
         if Business_Strategy["seats"] == 0:
             print("Business Strategy is full. Suggesting Marketing 101 instead")
         else:
-            fee + Business_Strategy["fee"]
+            fee += Business_Strategy["fee"]
 
     elif student_enrollment == "Marketing_101":
         if Marketing_101["seats"] == 0:
             print("Marketing 101 is full. Suggesting Business Strategy instead")
         else:
-            fee + Marketing_101["fee"]
+            fee += Marketing_101["fee"]
 
     elif student_enrollment == "Cybersecurity":
         if Cybersecurity["seats"] == 0:
             print("Cybersecurity is full. Suggesting Python Programming or Machine Learning instead")
         else:
-            fee + Cybersecurity["fee"]
+            fee += Cybersecurity["fee"]
 
     checkout = print(f"You are in {student_enrollment}, would you like to exit?(yes or no)")
     if checkout == "yes":
@@ -130,7 +102,11 @@ while True:
             fee * 0.95
         if student_enrollment == "Python Programming" or "Machine Learning" or "Cybersecurity":
             fee -= 20
-        print("Your total payment with discount is ${fee}")
+        print(input("Your total payment with discount is ${fee}"))
         break
     elif checkout == "no":
-        
+        continue
+#for i in course in courses.item():
+    #if course[seats] > 0
+        #print i
+        #break
